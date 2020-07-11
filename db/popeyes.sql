fdf106c8568bb43dee4acad0d16c176fb3ed87f2
Tran Van Tu <tuvantranhttta@gmail.com>
2020-07-11 16:50:25 +0700

    Cập nhật cơ sở dữ liệu

--- db/popeyes.sql
+++ db/popeyes.sql
@@ -18,23 +18,6 @@ SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for tbl_chi_tiet_don_hang
--- ----------------------------
-DROP TABLE IF EXISTS `tbl_chi_tiet_don_hang`;
-CREATE TABLE `tbl_chi_tiet_don_hang`  (
-  `id_don_hang` int(11) NOT NULL,
-  `id_san_pham` int(11) NOT NULL,
-  `don_gia` float(255, 0) NOT NULL,
-  `so_luong` int(11) NOT NULL,
-  `thanh_tien` float(255, 0) NOT NULL,
-  `ghi_chu` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id_don_hang`, `id_san_pham`) USING BTREE,
-  INDEX `id_san_pham`(`id_san_pham`) USING BTREE,
-  CONSTRAINT `tbl_chi_tiet_don_hang_ibfk_1` FOREIGN KEY (`id_don_hang`) REFERENCES `tbl_don_hang` (`id_don_hang`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `tbl_chi_tiet_don_hang_ibfk_2` FOREIGN KEY (`id_san_pham`) REFERENCES `tbl_san_pham` (`id_san_pham`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
 -- Table structure for tbl_danh_muc
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_danh_muc`;
@@ -60,24 +43,6 @@ INSERT INTO `tbl_danh_muc` VALUES (8, 'Phần ăn gia đình', NULL, NULL);
 INSERT INTO `tbl_danh_muc` VALUES (9, 'Thức uống và tráng miệng', NULL, NULL);
 INSERT INTO `tbl_danh_muc` VALUES (10, 'Combo cơm', NULL, NULL);
 INSERT INTO `tbl_danh_muc` VALUES (11, 'Kid meal', NULL, NULL);
-
--- ----------------------------
--- Table structure for tbl_don_hang
--- ----------------------------
-DROP TABLE IF EXISTS `tbl_don_hang`;
-CREATE TABLE `tbl_don_hang`  (
-  `id_don_hang` int(11) NOT NULL AUTO_INCREMENT,
-  `id_khach_hang` int(11) NULL DEFAULT NULL,
-  `id_nhan_vien` int(11) NOT NULL,
-  `tong_tien` float(255, 0) NOT NULL,
-  `trang_thai` int(4) NULL DEFAULT 0,
-  `ngay_dat` datetime(6) NULL DEFAULT NULL,
-  PRIMARY KEY (`id_don_hang`) USING BTREE,
-  INDEX `id_khach_hang`(`id_khach_hang`) USING BTREE,
-  INDEX `id_nhan_vien`(`id_nhan_vien`) USING BTREE,
-  CONSTRAINT `tbl_don_hang_ibfk_1` FOREIGN KEY (`id_khach_hang`) REFERENCES `tbl_khach_hang` (`id_khach_hang`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `tbl_don_hang_ibfk_2` FOREIGN KEY (`id_nhan_vien`) REFERENCES `tbl_nhan_vien` (`id_nhan_vien`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for tbl_khach_hang
@@ -123,6 +88,41 @@ CREATE TABLE `tbl_san_pham`  (
   INDEX `id_danh_muc`(`id_danh_muc`) USING BTREE,
   CONSTRAINT `tbl_san_pham_ibfk_1` FOREIGN KEY (`id_danh_muc`) REFERENCES `tbl_danh_muc` (`id_danh_muc`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for tbl_don_hang
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_don_hang`;
+CREATE TABLE `tbl_don_hang`  (
+  `id_don_hang` int(11) NOT NULL AUTO_INCREMENT,
+  `id_khach_hang` int(11) NULL DEFAULT NULL,
+  `id_nhan_vien` int(11) NOT NULL,
+  `tong_tien` float(255, 0) NOT NULL,
+  `trang_thai` int(4) NULL DEFAULT 0,
+  `ngay_dat` datetime(6) NULL DEFAULT NULL,
+  PRIMARY KEY (`id_don_hang`) USING BTREE,
+  INDEX `id_khach_hang`(`id_khach_hang`) USING BTREE,
+  INDEX `id_nhan_vien`(`id_nhan_vien`) USING BTREE,
+  CONSTRAINT `tbl_don_hang_ibfk_1` FOREIGN KEY (`id_khach_hang`) REFERENCES `tbl_khach_hang` (`id_khach_hang`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `tbl_don_hang_ibfk_2` FOREIGN KEY (`id_nhan_vien`) REFERENCES `tbl_nhan_vien` (`id_nhan_vien`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for tbl_chi_tiet_don_hang
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_chi_tiet_don_hang`;
+CREATE TABLE `tbl_chi_tiet_don_hang`  (
+  `id_don_hang` int(11) NOT NULL,
+  `id_san_pham` int(11) NOT NULL,
+  `don_gia` float(255, 0) NOT NULL,
+  `so_luong` int(11) NOT NULL,
+  `thanh_tien` float(255, 0) NOT NULL,
+  `ghi_chu` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_don_hang`, `id_san_pham`) USING BTREE,
+  INDEX `id_san_pham`(`id_san_pham`) USING BTREE,
+  CONSTRAINT `tbl_chi_tiet_don_hang_ibfk_1` FOREIGN KEY (`id_don_hang`) REFERENCES `tbl_don_hang` (`id_don_hang`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `tbl_chi_tiet_don_hang_ibfk_2` FOREIGN KEY (`id_san_pham`) REFERENCES `tbl_san_pham` (`id_san_pham`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tbl_san_pham
