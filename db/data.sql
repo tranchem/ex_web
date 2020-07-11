7361fde8cbfe8b9e679b41f0edde8f254b4677ab
Tran Van Tu <tuvantranhttta@gmail.com>
2020-07-11 16:56:02 +0700

    Cập nhật cơ sở dữ liệu

---
+++ db/data.sql
@@ -0,0 +1,20 @@
+-- ----------------------------
+-- Records of tbl_danh_muc
+-- ----------------------------
+INSERT INTO `tbl_danh_muc` VALUES (1, 'Món lẻ', NULL, NULL);
+INSERT INTO `tbl_danh_muc` VALUES (2, 'Món mới', NULL, NULL);
+INSERT INTO `tbl_danh_muc` VALUES (3, 'Gà giòn cay', NULL, NULL);
+INSERT INTO `tbl_danh_muc` VALUES (4, 'Gà giòn không cay', NULL, NULL);
+INSERT INTO `tbl_danh_muc` VALUES (5, 'Gà giòn không xương', NULL, NULL);
+INSERT INTO `tbl_danh_muc` VALUES (6, 'Bơ-gơ', NULL, NULL);
+INSERT INTO `tbl_danh_muc` VALUES (7, 'Hải sản', NULL, NULL);
+INSERT INTO `tbl_danh_muc` VALUES (8, 'Phần ăn gia đình', NULL, NULL);
+INSERT INTO `tbl_danh_muc` VALUES (9, 'Thức uống và tráng miệng', NULL, NULL);
+INSERT INTO `tbl_danh_muc` VALUES (10, 'Combo cơm', NULL, NULL);
+INSERT INTO `tbl_danh_muc` VALUES (11, 'Kid meal', NULL, NULL);
+
+-- ----------------------------
+-- Records of tbl_san_pham
+-- ----------------------------
+INSERT INTO `tbl_san_pham` VALUES (1, '1 MIẾNG GÀ GIÒN CAY', 36000, 1, 'https://popeyes.vn/media/catalog/product/cache/1/small_image/210x/9df78eab33525d08d6e5fb8d27136e95/_/8/_8ccdd1a622a3b94e72bf0fa457756b71ba0fbe8c38d48fe976_pimgpsh_fullsize_distr.png', 'Gà giòn Cajun tươi mới được ướp với công thức gia vị Louisiana (cay hoặc không cay), tẩm bột thủ công và rán chín cùng lớp phủ giòn tan đặc trưng của miền Nam nước Mỹ.');
+INSERT INTO `tbl_san_pham` VALUES (2, 'GÀ TẮM NƯỚC MẮM', 40000, 1, 'https://popeyes.vn/media/catalog/product/cache/1/small_image/210x/9df78eab33525d08d6e5fb8d27136e95/m/e/menu_spicy_redboat1pc.png', '1 miếng Gà Tắm Nước Mắm');
